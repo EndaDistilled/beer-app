a04f8f55637b217c553ae0af7f9a95178a20c475
Enda Brody <enda.brody@distilled.ie>
2020-11-21 13:10:32 +0000

    tests

--- frontend/src/components/BeerApp.js
+++ frontend/src/components/BeerApp.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ListBeersComponent from "./ListBeersComponent";
+import BeerComponent from "./BeerComponent";
 
 class BeerApp extends Component {
     render() {
         return (<>
             <h1>Beer Application</h1>
             <ListBeersComponent/>
+            <BeerComponent/>
              </>
         )
     }
