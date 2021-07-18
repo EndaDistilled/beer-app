ecbd1dffa569c7f901903d7607ff0be287537c19
EndaDistilled <enda.brody@distilled.ie>
2021-07-18 12:00:28 +0100

    add db ann pre populate  db via docker

--- 1-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE beers
-(
-    beer_id  SERIAL PRIMARY KEY,
-    beer_name   varchar(30) NOT NULL,
-    brewery_location varchar(30) NOT NULL,
-    beer_description varchar(500) NULL,
-    alcohol_percentage float NOT NULL
-);
