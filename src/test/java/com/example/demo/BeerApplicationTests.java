3c88cd3f0fc78f87dcd9287488a82994ba7c4dc6
EndaDistilled <enda.brody@distilled.ie>
2021-07-20 09:14:41 +0100

    add docker files and test

--- src/test/java/com/example/demo/BeerApplicationTests.java
+++ src/test/java/com/example/demo/BeerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 class BeerApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
-    void contextLoads() {
+    void shouldLoadApplicationCtxAndConfig() {
+        assertThat(applicationContext).isNotNull();
     }
 
 }
